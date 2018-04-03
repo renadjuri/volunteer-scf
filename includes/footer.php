955f1c5d37b7baa579a65be5314937fadd50d27f
renad <renad@Renad>
2018-04-03 16:59:33 +0300

    header+footer

--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,7 @@
     <div class="LinksFooter">
         <a href="index.php">الصفحة الرئيسية</a> | <!-- Link to home page -->
         <a href="events.php">الفعاليات</a> | <!-- Link to events page -->
-        <a href="Contact_us.php">تواصل معنا</a> |<!-- Link to Contact us page -->
+        <a href="Contact_us.php">تواصل معنا</a> <!-- Link to Contact us page -->
      
     </div> 
 
