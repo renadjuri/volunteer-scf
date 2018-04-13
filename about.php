d3027b258b7273cb56c07091501b9cf098b7e100
bushra <bushra@192.168.8.103>
2018-04-14 00:46:00 +0300

    تعديل

--- about.php
+++ about.php
@@ -18,6 +18,71 @@ include("includes/Header.php"); // the header of the page
             <br>
             <br>
         </div>
+    <div class="about-section">
+		<div class="container">
+			<div class="site-title text-center">
+                                        
+				<p class="lead">نشأت جمعية السرطان السعودية تاريخ ١٩ صفر ١٤٢٤ هـ الموافق ٢١ ابريل ٢٠٠٣ م</p>
+				<p class="lead">تعتمد الجمعية على ركائز أساسية لتحقيق رؤيتها ورسالتها وهم: الداعمون، المتطوعون، المجتمع، الكوادر الطبية، المرضى،العاملون</p>
+			</div>
+                    <br><br><br><br>
+			<div class="container">
+			<div class="about-inner-section">	
+			    <div class="col-md-6 about-right">
+				<img src="images/ab1.png" alt=" ">
+			</div>
+				<div class="col-md-6 about-inner-column">
+					<h4>رؤيتنا </h4>
+                                        <p>تحقيق مكانه متميزة وفقاً للمعايير الدولية في مجال مكافحة مرض السرطان والوصول لمجتمع صحي على مستوى المملكة</p>
+				
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		</div>
+	</div>
+    
+        <br><br>
+    <!--- -->
+
+<div class="about-section">
+		<div class="container">
+			<div class="site-title text-center">
+			</div>
+			<div class="about-inner-section">	
+			    <div class="col-md-6 about-right">
+				<img src="images/ab2.png" alt=" ">
+			</div>
+				<div class="col-md-6 about-inner-column">
+					<h4>رسالتنا </h4>
+					<p>العمل على زيادة الوعي الصحي حول مرض السرطان واهمية الوقاية والكشف المبكر عن المرض وتنفيذ برامج صحية تستهدف كافة شرائح المجتمع تهدف الى خفض معدلات الإصابة بالمرض </p>
+				</div>
+				
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+    <br><br>
+  <div class="about-section">
+		<div class="container">
+			<div class="site-title text-center">
+			</div>
+			<div class="about-inner-section">	
+			    <div class="col-md-6 about-right">
+				<img src="images/ab3.png" alt=" ">
+			</div>
+				<div class="col-md-6 about-inner-column">
+					<h4>برامجنا </h4>
+					<p>برنامج الكشف المبكر عن السرطان -</p>
+                                        <p>خدمات مساندة المرضى -</p>
+                                        <p>تقديم محاضرات وندوات ودورات وورش تدريبية عن التوعية بالسرطان -</p>
+                                        <p>برامج الإبتعاث وتطوير الكوادر الطبية  -</p>
+                                        <p>برنامج الحملات التوعوية المواكبة للأشهر العالمية للتوعية بأمراض السرطان  -</p>
+				</div>
+				
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
   </body>
 <br>
 <br>
@@ -27,11 +92,7 @@ include("includes/Header.php"); // the header of the page
 <br>
 <br>
 <br>
-<br>
-<br>
-<br>
-<br>
-<br>
+
 
 <!--Footer of the page -->
 
